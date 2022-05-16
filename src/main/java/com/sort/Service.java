089ee9bfaa70b7e1d0d4a9a08a48c026037f8386
Ícaro Targino <icaro_rique123@hotmail.com>
2022-05-16 19:22:09 -0300

    Finished -> Merge.java || This is the Java implementation of MergeSort, we split the list until it become a single element list, order the elements and go up the tree of divided lists ordering at each step.

--- src/main/java/com/sort/Service.java
+++ src/main/java/com/sort/Service.java
@@ -16,8 +16,11 @@ public class Service {
     }
 
     public void toSort() {
-        this.toSortList = new ArrayList<>(Arrays.asList(10.0, 6.0, 4.0, 17.0, 5.0, 12.0, -15.0, 0.0));
-        System.out.println(sortMethod.getClass().getSimpleName() +"\t:: ORIGINAL\t ::\t"+ this.toSortList);
-        System.out.println(sortMethod.getClass().getSimpleName() +"\t:: ORDERED\t ::\t"+ this.sortMethod.sort(toSortList));
+        this.toSortList = new ArrayList<>(List.of(10.0, 6.0, 4.0, 17.0, 5.0, 12.0, -15.0, 0.0));
+        //System.out.println(sortMethod.getClass().getSimpleName() +"\t:: ORIGINAL\t ::\t"+ this.toSortList);
+        //System.out.println(sortMethod.getClass().getSimpleName() +"\t:: ORDERED\t ::\t"+ this.sortMethod.sort(toSortList));
+
+        System.out.printf("%-10s :: ORIGINAL :: %s \n",sortMethod.getClass().getSimpleName(),this.toSortList);
+        System.out.printf("%-10s :: ORDERED  :: %s \n",sortMethod.getClass().getSimpleName(),this.sortMethod.sort(toSortList));
     }
 }
--- src/main/java/com/sort/algorithm/Merge.java
+++ src/main/java/com/sort/algorithm/Merge.java
@@ -1,7 +1,6 @@
 package com.sort.algorithm;
 
 import com.sort.algorithm.contract.Sort;
-import com.sort.algorithm.contract.Swap;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,30 +9,43 @@ public class Merge implements Sort {
 
     @Override
     public <T extends Double> List<T> sort(List<T> list) {
-        System.out.println(list);
-        if(list.size() < 2) {
+
+        if(list.size() <= 1) {
             return list;
         }
-        List<T> leftList = this.sort(list.subList(0, (list.size()) / 2));
+
+        List<T> leftList = this.sort(list.subList(0, (list.size()/2)));
         List<T> rightList = this.sort(list.subList(list.size()/2, list.size()));
 
-        List<T> mergeList = this.merge(leftList, rightList);
+        List<T> mergeList = this.merge(list, leftList, rightList);
 
         return mergeList;
     }
 
-    private <T extends Double> List<T> merge(List<T> leftList, List<T> rightList) {
+    private <T extends Double> List<T> merge(List<T> list, List<T> leftList, List<T> rightList) {
         List<T> aux = new ArrayList<>();
 
-        for(int i = 0; i < leftList.size(); i++ ){
-            for (int j = 0; j < rightList.size(); j++){
-                if( leftList.get(i).doubleValue() < rightList.get(j).doubleValue()){
-                    aux.add(leftList.get(i));
-                } else {
-                    aux.add(rightList.get(j));
-                }
+        Integer i = 0;
+        Integer j = 0;
+
+        while(i < leftList.size() && j < rightList.size()) {
+            if(leftList.get(i).doubleValue() < rightList.get(j).doubleValue()){
+                aux.add(leftList.get(i));
+                i++;
+            } else {
+                aux.add(rightList.get(j));
+                j++;
             }
         }
+        while (i < leftList.size()){
+            aux.add(leftList.get(i));
+            i++;
+        }
+        while (j < rightList.size()){
+            aux.add(rightList.get(j));
+            j++;
+        }
+
         return aux;
     }
 }
